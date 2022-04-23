e21ff83d2762714ca0b007a865c5ea1d89ee7acc
passing breeze <5461413+passing-comet@users.noreply.github.com>
2022-04-23 17:44:37 -0400

    set gmmk pro layouts and undef rgb modes

--- keyboards/gmmk/pro/ansi/keymaps/passing-comet/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/passing-comet/keymap.c
@@ -32,23 +32,6 @@ enum custom_keycodes {
   RGB_I_C,
 };
 
-// Shift + esc = ~
-const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRV));
-
-// Ctrl + esc = `
-const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_CTRL, KC_ESC, KC_GRV);
-
-// Shift + bspc = del
-const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-
-// This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &tilde_esc_override,
-    &grave_esc_override,
-    &delete_key_override,
-    NULL // Null terminate the array of overrides!
-};
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12      Prt           Rotary(Mute)
@@ -71,21 +54,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,          KC_MUTE,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN1),KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN1] = LAYOUT(
         KC_PWR,  KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          RGB_TOG,
         _______, RGB_I_S, RGB_I_G, RGB_I_C, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        RGB_TOG, ALT_F4,  CTRL_W,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,            _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, RGB_MOD, _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
+        _______, ALT_F4,  CTRL_W,  RGB_SPI, RGB_TOG, RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_PSCR, KC_SLCK, KC_PAUS, RESET,            KC_MUTE,
+        _______, _______, RGB_SPI, _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______,          KC_MPLY,          KC_MUTE,
+        _______,          _______, _______, _______, RGB_VAI, _______, RGB_RMOD,RGB_MOD,_______,  _______, _______,          _______, KC_VOLU, _______,
+        _______, _______, KC_MPRV,                            KC_MPLY,                            KC_MNXT, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
     ),
 
     [_FN2] = LAYOUT(
@@ -108,6 +91,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
+// Shift + esc = ~
+const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRV));
+
+// Ctrl + esc = `
+const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_CTRL, KC_ESC, KC_GRV);
+
+// Shift + bspc = del
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &tilde_esc_override,
+    &grave_esc_override,
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
+};
+
 bool led_update_user(led_t led_state) {
     if (led_state.caps_lock) {
         if (!rgb_matrix_is_enabled()) {
@@ -124,28 +124,38 @@ bool led_update_user(led_t led_state) {
     return true;
 }
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    if (layer_state_is(_FN1)) {
-        if (!rgb_matrix_is_enabled()) {
-            /* Turn ON the RGB Matrix for _FN1 */
-            rgb_matrix_set_flags(LED_FLAG_NONE);
-            rgb_matrix_enable();
-        }
-    } else if (rgb_matrix_get_flags() == LED_FLAG_NONE) {
-        /* RGB Matrix was only ON because of _FN1. Turn it OFF. */
-        rgb_matrix_set_flags(LED_FLAG_ALL);
-        rgb_matrix_disable();
-    }
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//     if (layer_state_is(_FN1)) {
+//         if (!rgb_matrix_is_enabled()) {
+//             /* Turn ON the RGB Matrix for _FN1 */
+//             rgb_matrix_set_flags(LED_FLAG_NONE);
+//             rgb_matrix_enable();
+//         }
+//     } else if (rgb_matrix_get_flags() == LED_FLAG_NONE) {
+//         /* RGB Matrix was only ON because of _FN1. Turn it OFF. */
+//         rgb_matrix_set_flags(LED_FLAG_ALL);
+//         rgb_matrix_disable();
+//     }
 
-    return state;
-}
-
+//     return state;
+// }
 
 #ifdef RGB_MATRIX_ENABLE
+    // RGB led number layout, function of the key
+    //
+    //  67, led 01   0, ESC    6, F1      12, F2      18, F3   23, F4   28, F5      34, F6   39, F7   44, F8      50, F9   56, F10   61, F11    66, F12    69, Prt       Rotary(Mute)   68, led 12
+    //  70, led 02   1, ~      7, 1       13, 2       19, 3    24, 4    29, 5       35, 6    40, 7    45, 8       51, 9    57, 0     62, -_     78, (=+)   85, BackSpc   72, Home       71, led 13
+    //  73, led 03   2, Tab    8, Q       14, W       20. E    25, R    30, T       36, Y    41, U    46, I       52, O    58, P     63, [{     89, ]}     93, \|        75, PgUp       74, led 14
+    //  76, led 04   3, Caps   9, A       15, S       21, D    26, F    31, G       37, H    42, J    47, K       53, L    59, ;:    64, '"                96, Enter     86, PgDn       77, led 15
+    //  80, led 05   4, Sh_L   10, Z      16, X       22, C    27, V    32, B       38, N    43, M    48, ,<      54, .>   60, /?               90, Sh_R   94, Up        82, End        81, led 16
+    //  83, led 06   5, Ct_L   11,Win_L   17, Alt_L                     33, SPACE                     49, Alt_R   55, FN             65, Ct_R   95, Left   97, Down      79, Right      84, led 17
+    //  87, led 07                                                                                                                                                                      88, led 18
+    //  91, led 08
+
     // Called on powerup and is the last _init that is run
-    void keyboard_post_init_keymap(void) {
-        rgb_matrix_mode(RGB_MATRIX_CUSTOM_indicator_gradient);
-    }
+    // void keyboard_post_init_keymap(void) {
+    //     rgb_matrix_mode(RGB_MATRIX_CUSTOM_indicator_gradient);
+    // }
 
     // Runs on key event
     bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -173,43 +183,27 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         // Turn on glow when CAPS LOCK is activated
         if (host_keyboard_led_state().caps_lock) {
-            int leds[6] = {2, 3, 4, 8, 9, 10};
-            int length = (int)(sizeof(leds)/sizeof(leds[0]));
+            int leds[5] = {3, 70, 73, 76, 80};
             int i;
+            int size = (int)(sizeof(leds)/sizeof(leds[0]));
 
-            for (i = 0; i < length; i++) {
-                rgb_matrix_set_color(leds[i], RGB_RED);
+            for (i = 0; i < size; i++) {
+                RGB_MATRIX_INDICATOR_SET_COLOR(leds[i], 255, 255, 255);
             }
         }
 
-        // Layer indicators on right side lights.
-        if (layer_state_is(_FN1)) {
-            int leds[6] = {49, 54, 55, 60, 65, 90};
-            int length = (int)(sizeof(leds)/sizeof(leds[0]));
+        // Turn on glow when FN is activated
+        uint8_t layer = get_highest_layer(layer_state);
+        if (layer > 0) {
+            int leds[5] = {55, 71, 74, 77, 81};
             int i;
+            int size = (int)(sizeof(leds)/sizeof(leds[0]));
 
-            for (i = 0; i < length; i++) {
-                rgb_matrix_set_color(leds[i], RGB_RED);
+            for (i = 0; i < size; i++) {
+                RGB_MATRIX_INDICATOR_SET_COLOR(leds[i], 255, 255, 255);
             }
+
+            RGB_MATRIX_INDICATOR_SET_COLOR(layer, 255, 255, 255);
         }
-    }
-
-    void suspend_power_down_user(void) {
-        rgb_matrix_set_suspend_state(true);
-    }
-
-    void suspend_wakeup_init_user(void) {
-        rgb_matrix_set_suspend_state(false);
     }
 #endif  // RGB_MATRIX_ENABLE
-
-// RGB led number layout, function of the key
-//
-//  67, led 01   0, ESC    6, F1      12, F2      18, F3   23, F4   28, F5      34, F6   39, F7   44, F8      50, F9   56, F10   61, F11    66, F12    69, Prt       Rotary(Mute)   68, led 12
-//  70, led 02   1, ~      7, 1       13, 2       19, 3    24, 4    29, 5       35, 6    40, 7    45, 8       51, 9    57, 0     62, -_     78, (=+)   85, BackSpc   72, Home       71, led 13
-//  73, led 03   2, Tab    8, Q       14, W       20. E    25, R    30, T       36, Y    41, U    46, I       52, O    58, P     63, [{     89, ]}     93, \|        75, PgUp       74, led 14
-//  76, led 04   3, Caps   9, A       15, S       21, D    26, F    31, G       37, H    42, J    47, K       53, L    59, ;:    64, '"                96, Enter     86, PgDn       77, led 15
-//  80, led 05   4, Sh_L   10, Z      16, X       22, C    27, V    32, B       38, N    43, M    48, ,<      54, .>   60, /?               90, Sh_R   94, Up        82, End        81, led 16
-//  83, led 06   5, Ct_L   11,Win_L   17, Alt_L                     33, SPACE                     49, Alt_R   55, FN             65, Ct_R   95, Left   97, Down      79, Right      84, led 17
-//  87, led 07                                                                                                                                                                      88, led 18
-//  91, led 08
