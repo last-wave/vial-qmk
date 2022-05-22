d4f1d98a67c145fdd15fba48ca508d2c95ffddd5
passing comet <5461413+passing-comet@users.noreply.github.com>
2022-05-21 21:18:27 -0400

    add kbd67 lite

---
+++ keyboards/kbdfans/kbd67/mkiirgb/v3/keymaps/passing-comet/rules.mk
@@ -0,0 +1,19 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+
+# Current configuration is approaching the maximum for the firmware - 25032/26624 (99%, 250 bytes free)
+# Additional space can be achived by disabling LED animations in config.h - 20936/266244 (78%, 5688 bytes free)
+
+VIAL_INSECURE = yes # ~+344 bytes
+
+QMK_SETTINGS = no # ~2560 bytes
+TAP_DANCE_ENABLE = yes # ~2196 bytes
+COMBO_ENABLE = no # ~2408 bytes
+
+# SPACE_CADET_ENABLE = no # ~324 bytes
+# MAGIC_ENABLE = no # ~584 bytes
+# AUTO_SHIFT_ENABLE = yes # ~2,560 bytes
+KEY_OVERRIDE_ENABLE = yes # ~1,614 bytes
+
+# 26524/26624 (99%, 100 bytes free)
