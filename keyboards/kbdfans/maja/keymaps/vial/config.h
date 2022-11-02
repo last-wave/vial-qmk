d268bd3597ad1160615e3e444eef03bbb532df16
passing comet <5461413+passing-comet@users.noreply.github.com>
2022-11-02 15:58:57 -0400

    add maja

---
+++ keyboards/kbdfans/maja/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0D, 0xF4, 0x5B, 0x65, 0xBA, 0xDD, 0x55, 0x2E}
+#define VIAL_TAP_DANCE_ENTRIES 16
+
+#define NO_ACTION_ONESHOT
